ec0ba950b67f52a0e33ff5dec985f75627bd816c
ibrahim <ibrahimjp630@gmail.com>
2025-04-07 21:15:23 +0530

    Initial commit

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -57,7 +57,7 @@ const Hero = () => {
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Ibrahim, a full stack developer.
+            Hi! I'm Ibrahim, a full stack developer.
           </p>
 
           <a href="#about">
