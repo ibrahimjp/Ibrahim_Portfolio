ffd0ba5de82caa4b1ab8166632bc95f726379ea6
ibrahim <ibrahimjp630@gmail.com>
2025-04-23 22:30:52 +0530

    fix: build errors and warnings

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 git init
 git add README.md
 git add .
-git commit -m "first commit"
+git commit -m "Deployment"
 git branch -M main
 git remote add origin https://github.com/ibrahimjp/ibrahim_portfolio.git
 git push -u origin main
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -43,7 +43,7 @@ const Hero = () => {
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-           Welcome To Ibrahim's Portfolio
+           Welcome To Ibrahim Portfolio
           </p>
 
           {/**
@@ -57,7 +57,7 @@ const Hero = () => {
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I'm Ibrahim, a full stack developer.
+            Hi! I am Ibrahim, a full stack developer.
           </p>
 
           <a href="#about">
